910e5e589f3115d07674610ceff907dbadc5a823
Srinidhi Akkanapragada <nidhia@Srinidhis-MacBook-Pro.local>
2026-06-11 11:43:27 +0530

    Move comedian data into separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
 import { useState } from 'react'
-
-const comedians = [
-  { id: 1, name: "Zakir Khan", style: "Self-deprecating", bio: "The OG sakht launda of Indian comedy." },
-  { id: 2, name: "Kanan Gill", style: "Self-deprecating", bio: "The king of thoughtprovoking comedy in India." },
-  { id: 3, name: "Vir Das", style: "Political", bio: "The global face of Indian comedy." },
-  { id: 4, name: "Kenny Sebastian", style: "Observational", bio: "The master of everyday comedy." },
-  { id: 5, name: "Biswa Kalyan Rath", style: "Dark Humour", bio: "The dark knight of Indian comedy." }, 
-  {id: 6, name: "Abish Mathew", style: "Observational", bio: "The singing sensation of Indian comedy." },
-]
+import { comedians } from './data/comedians'
 
 const filters = ['All', 'Dark Humour', 'Observational', 'Self-deprecating', 'Political']
 
---
+++ src/data/comedians.js
@@ -0,0 +1,38 @@
+export const comedians = [
+  {
+    id: 1,
+    name: "Zakir Khan",
+    style: "Self-deprecating",
+    bio: "The OG sakht launda of Indian comedy."
+  },
+  {
+    id: 2,
+    name: "Kanan Gill",
+    style: "Self-deprecating",
+    bio: "The king of thought provoking comedy in India."
+  },
+  {
+    id: 3,
+    name: "Vir Das",
+    style: "Political",
+    bio: "The global face of Indian comedy."
+  },
+  {
+    id: 4,
+    name: "Kenny Sebastian",
+    style: "Observational",
+    bio: "The master of everyday comedy."
+  },
+  {
+    id: 5,
+    name: "Biswa Kalyan Rath",
+    style: "Dark Humour",
+    bio: "The dark knight of Indian comedy."
+  },
+  {
+    id: 6,
+    name: "Abish Mathew",
+    style: "Observational",
+    bio: "The singing sensation of Indian comedy."
+  }
+]
